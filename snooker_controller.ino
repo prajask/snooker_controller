1165342178402d59a1fad2cbcb9770448cb1f605
Prajas Kadepurkar <kadepurkarprajas@gmail.com>
2024-03-10 11:01:28 -0700

    Changes for speed of direction

--- snooker_controller.ino
+++ snooker_controller.ino
@@ -11,8 +11,14 @@
 // #define PRESSURE_PIN A3
 #define HIT_PIN 3
 
-#define LEFT "left"
-#define RIGHT "right"
+#define LEFT_SLOW "left_slow"
+#define LEFT_MEDIUM "left_medium"
+#define LEFT_FAST "left_fast"
+
+#define RIGHT_SLOW "right_slow"
+#define RIGHT_MEDIUM "right_medium"
+#define RIGHT_FAST "right_fast"
+
 #define HIT "hit"
 
 int yValue = 0; // To store value of the Y axis
@@ -32,12 +38,28 @@ void loop() {
   while(digitalRead(HIT_PIN) == HIGH){
     // Serial.println(pressureValue);
     yValue = analogRead(VRY_PIN);
-    if(yValue >= 550){
-      Serial.println(LEFT);
+    if(yValue >= 550 && yValue < 707){
+      Serial.println(LEFT_SLOW);
       delay(500);
     }
-    else if(yValue <=500){
-      Serial.println(RIGHT);
+    else if(yValue >= 707 && yValue < 864){
+      Serial.println(LEFT_MEDIUM);
+      delay(500);
+    }
+    else if(yValue >= 864 && yValue < 1023){
+      Serial.println(LEFT_FAST);
+      delay(500);
+    }
+    else if(yValue <= 500 && yValue > 334){
+      Serial.println(RIGHT_SLOW);
+      delay(500);
+    }
+    else if(yValue <= 334 && yValue > 168){
+      Serial.println(RIGHT_MEDIUM);
+      delay(500);
+    }
+    else if(yValue <= 168 && yValue > 0){
+      Serial.println(RIGHT_FAST);
       delay(500);
     }
   }
